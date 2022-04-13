c757129228d0787507dc0894da788875b577d2f0
Clement Appoles <CAppoles@oldmutual.com>
2022-04-13 10:37:34 +0200

    Updates

--- src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import carReducer from "../features/car/carslice"
-export const store = configureStore({
-  reducer: {
-    car: carReducer,
-  },
-});
